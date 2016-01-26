6e1d2583a950548525bc31a75d81dd63fe4fb12b
herman <hm3cheun83@gmail.com>
2016-01-26 01:01:35 +0000

    stock list and view profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-devise_for :users, :controllers => { :registration => "user/registrations" }
+devise_for :users, :controllers => { :registrations => "user/registrations" }
 resources :user_stocks, except: [:show, :edit, :update]
 resources :users, only: [:show]
 resources :friendships
@@ -10,6 +10,8 @@ root 'welcome#index'
 get 'my_portfolio', to: "users#my_portfolio"
 get 'search_stocks', to: 'stocks#search'
 get 'my_friends', to: "users#my_friends"
+get 'search_friends', to: 'users#search'
+post 'add_friend', to: 'users#add_friend'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
